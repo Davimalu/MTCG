6f92f23daa574cc701499e8f4123d10aa1930410
Davimalu <accounts@zeugner.dev>
2024-09-28 13:24:44 +0200

    Add cardService and getRandomCard()

---
+++ MTCG/Logic/CardService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MTCG.Models;
+
+namespace MTCG.Logic
+{
+    public class CardService
+    {
+        public CardService()
+        {
+        
+        }
+
+        public Card GetRandomCard()
+        {
+            // Generate random card
+            Random random = new Random();
+
+            int damage = random.Next(1, 101);
+            ElementType type = (ElementType)random.Next(0, 3);
+
+            // Either monster or spell card
+            if (random.Next() % 2 == 0)
+            {
+
+                return new MonsterCard("Monster", damage, type);
+            }
+            else
+            {
+                return new SpellCard("Spell", damage, type);
+            }
+        }
+    }
+}
--- MTCG/Models/Package.cs
+++ MTCG/Models/Package.cs
@@ -1,15 +1,22 @@
-﻿namespace MTCG.Models
+﻿using MTCG.Logic;
+
+namespace MTCG.Models
 {
     internal class Package
     {
-        private Card[] cards; // A package consists of 5 cards
+        private List<Card> cards = new List<Card>(5); // A package consists of 5 cards
         private int price = 5; // The price of a package is 5 coins
+
+        private CardService CardService = new CardService();
 
         // Constructor
         public Package()
         {
-            // Fill package with 5 cards
-            cards = new Card[5];
+            // Fill package with 5 random cards
+            for (int i = 0; i < 5; i++)
+            {
+                cards.Add(CardService.GetRandomCard());
+            }
         }
     }
 }
