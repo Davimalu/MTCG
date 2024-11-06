de4d0d7a762bcedc6b317a6294c444b241541e41
Davimalu <accounts@zeugner.dev>
2024-11-06 08:39:14 +0100

    Add additional unit tests for AuthService

--- MTCGTests/TestAuthService.cs
+++ MTCGTests/TestAuthService.cs
@@ -4,9 +4,12 @@ using MTCG.Models;
 using MTCG.Repository;
 using NSubstitute;
 using NSubstitute.ReturnsExtensions;
+using BCrypt.Net;
 
 namespace MTCGTests
 {
+    using BCrypt.Net;
+
     public class TestAuthService
     {
         private IUserRepository userRepositoryMock;
@@ -15,11 +18,13 @@ namespace MTCGTests
         [SetUp]
         public void Setup()
         {
+            var hashedPassword = BCrypt.HashPassword("testPassword");
+
             // Create mock for testing
             userRepositoryMock = Substitute.For<IUserRepository>();
 
             // Define mock behaviour
-            userRepositoryMock.GetUserByName("testUser").Returns(new User("testUser", "testPassword"));
+            userRepositoryMock.GetUserByName("testUser").Returns(new User("testUser", hashedPassword));
             userRepositoryMock.GetUserByName("anotherTestUser").ReturnsNull();
 
             // Dependency Injection in AuthService
@@ -73,11 +78,42 @@ namespace MTCGTests
             var username = "anotherTestUser";
 
             //// Act ////
-            var result = authService.Login("anotherTestUser", "doesn'tExist");
+            var result = authService.Login(username, "doesn'tExist");
 
             //// Assert ////
             userRepositoryMock.Received(1).GetUserByName(username);
             Assert.Null(result);
+        }
+
+        [Test]
+        public void testLogin_ReturnsNullIfPasswordIsInvalid()
+        {
+            //// Arrange ////
+            var username = "testUser";
+            var password = "wrongPassword";
+
+            //// Act ////
+            var result = authService.Login(username, password);
+
+            //// Assert ////
+            userRepositoryMock.Received(1).GetUserByName(username);
+            Assert.Null(result);
+        }
+        
+        [Test]
+        public void testLogin_ReturnsUserIfPasswordIsValid()
+        {
+            //// Arrange ////
+            var username = "testUser";
+            var password = "testPassword"; // Correct password
+
+            //// Act ////
+            var result = authService.Login(username, password);
+
+            //// Assert ////
+            userRepositoryMock.Received(1).GetUserByName(username);
+            Assert.NotNull(result);
+            Assert.That(username, Is.EqualTo(result.Username), "Should return the user that logged in");
         }
     }
 }
