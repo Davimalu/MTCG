cb4cc90ea0351369f22d2e9eb14fcade03bb9916
Davimalu <accounts@zeugner.dev>
2024-12-20 11:08:39 +0100

    Refactor StatsEndpoint

--- MTCG/Endpoints/StatsEndpoint.cs
+++ MTCG/Endpoints/StatsEndpoint.cs
@@ -1,41 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using MTCG.HTTP;
+﻿using MTCG.HTTP;
 using MTCG.Interfaces;
 using MTCG.Logic;
 using MTCG.Models;
+using System.Net.Sockets;
+using System.Text.Json;
 
 namespace MTCG.Endpoints
 {
     public class StatsEndpoint : IHttpEndpoint
     {
-        private readonly UserService _userService = UserService.Instance;
+        private readonly IUserService _userService = UserService.Instance;
         private readonly IHeaderHelper _headerHelper = new HeaderHelper();
 
         public (int, string?) HandleRequest(TcpClient? client, HTTPHeader headers, string? body)
         {
-            // Get stats of user
-            if (headers.Method == "GET")
+            // Check if user is authorized
+            string token = _headerHelper.GetTokenFromHeader(headers)!;
+            User? user = _userService.GetUserByToken(token);
+
+            if (user == null)
             {
-                // Check if user is authorized
-                string token = _headerHelper.GetTokenFromHeader(headers)!;
-                User? user = _userService.GetUserByToken(token);
-
-                if (user == null)
-                {
-                    return (401, JsonSerializer.Serialize("User not authorized"));
-                }
-
-                string json = JsonSerializer.Serialize(user.Stats);
-                return (200, json);
+                return (401, JsonSerializer.Serialize("User not authorized"));
             }
 
-            return (405, "Method not allowed");
+            switch (headers.Method)
+            {
+                // Get user stats
+                case "GET":
+                    return (200, JsonSerializer.Serialize(user.Stats));
+                default:
+                    return (405, "Method not allowed");
+            }
         }
     }
 }
